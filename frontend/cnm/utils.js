58fe0a00d49e2b1a47fb52c48b7cbb3522ef6ffa
betterdatas <159976316+betterdatas@users.noreply.github.com>
2024-02-14 15:37:59 +0800

    Create utils.js

---
+++ frontend/cnm/utils.js
@@ -0,0 +1,8 @@
+// frontend/cnm/utils.js
+
+function formatDate(date) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export { formatDate };

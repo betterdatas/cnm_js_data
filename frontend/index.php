c609e79e3bc85ed01b1827ab190f8cc705988c7e
betterdatas <159976316+betterdatas@users.noreply.github.com>
2024-02-14 16:21:29 +0800

    Create index.php

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<!-- frontend/index.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/styles/main.css">
+  <title>Code Notation Manager</title>
+</head>
+
+<body>
+  <h1>Welcome to Code Notation Manager</h1>
+
+  <script type="module" src="js/main.js"></script>
+</body>
+
+</html>
